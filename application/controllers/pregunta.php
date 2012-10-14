0d769ff872eeeaf477524abf84eccd781d07e841
blzkz <sergiobl88@gmail.com>
2012-10-14 02:42:11 +0200

    added unique view for a question

--- application/controllers/pregunta.php
+++ application/controllers/pregunta.php
@@ -17,10 +17,22 @@ class Pregunta extends CI_Controller {
 
 	public function control()
 	{
-		
+		$db = mongo_connect();
+		$q = $db->pregunta->save(array('titulo' => $_POST['titulo'], 'pregunta' => $_POST['pregunta']));
+		foreach ($db->pregunta->find()->sort(array('_id'=> -1))->limit(1) as $obj) {
+    		$id = $obj[_id];
+		}
+		redirect("/pregunta/ver/".$id);
+		exit;
 	}
 
+	// Para ver una pregunta en concreto
 	public function ver()
+	{
+
+	}
+
+	public function lista()
 	{
 		$this->load->view('header');
 		$this->load->view('lista', $data);
--- application/views/form_pregunta.php
+++ application/views/form_pregunta.php
@@ -3,6 +3,6 @@ echo form_open('pregunta/control');
 echo form_label('Título')."<br>";
 echo form_input('titulo')."<br>";
 echo form_label('Pregunta')."<br>";
-echo form_textarea('Pregunta')."<br>";
+echo form_textarea('pregunta')."<br>";
 echo form_submit('Enviar', 'Enviar');
 echo form_close();
